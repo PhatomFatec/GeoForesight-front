147a4d428950d803f8a6609120008c799505448b
Marcus Vinícius <marcus.varocha@gmail.com>
2023-09-20 23:22:50 +0000

    GF-37: Criação componente search

--- geo-foresight/components/search/search.tsx
+++ geo-foresight/components/search/search.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div className='bg-gray-300 m-7 h-12 flex p-2 rounded-md'>
-      <p></p>
-      <input className='rounded-md outline-none p-1' type="text" />
-      <button>Buscar</button>
+    <div className='bg-[#D9D9D9] m-7 h-12 flex py-2 rounded-md items-center justify-between'>
+      <div className="flex flex-row items-center">
+        <p className='mr-5 ml-5'>Código da Gleba</p>
+        <input className='rounded-md outline-none p-1 mr-5' type="text" />
+      </div>
+      <button className='bg-[#376CB9] text-white h-full w-36 rounded-md mr-5'>Localizar</button>
     </div>
   )
 }
