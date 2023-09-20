ae6263ed606dfbd91f6f98a0cb177805c704b19c
Marcus Vinícius <marcus.varocha@gmail.com>
2023-09-20 00:52:16 +0000

    GF-37: [frontend] Criar componente search

---
+++ geo-foresight/components/search/search.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className='bg-gray-300 m-7 h-12 flex p-2 rounded-md'>
+      <p></p>
+      <input className='rounded-md outline-none p-1' type="text" />
+      <button>Buscar</button>
+    </div>
+  )
+}
+
+export default Search
